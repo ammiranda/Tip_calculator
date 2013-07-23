3ef3745cb92415d8fab690f3f4485c954f2cbfd3
Alexander <alexandermichaelmiranda@gmail.com>
2013-07-23 10:40:20 -0400

    decimal update

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,12 +1,13 @@
 meal = 44.50
 tax = 0.0675
 tip = 0.15
+percent_tip = tip * 100
 tax_value = meal * tax
 meal_with_tax = meal + tax_value
 tip_value = meal_with_tax * tip
 total = meal_with_tax + tip_value
 
-print("Base cost of the meal: %r" % meal)
-print("Tax amount on the meal: %r" % tax_value)
-print("Tip amount on the meal: %r" % tip_value)
-print("Total meal cost: %r" % total)
+print "The base cost of your meal was $%.2f" % meal
+print "You need to pay $%.2f for tax" % tax_value
+print "Tipping at a rate of %.0f" + '%' + ", you should leave $%.2f for a tip." % (percent_tip , tip_value)
+print "The grand total of your meal is $%.2f" % (total)
